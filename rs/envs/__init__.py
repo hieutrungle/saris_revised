b81ea15333062e73e725ac22ba24154d23584006
HieuLe <hieult1996@gmail.com>
2025-08-04 12:16:49 -0500

    add column-based drl

--- rs/envs/__init__.py
+++ rs/envs/__init__.py
@@ -2,10 +2,12 @@ from rs.envs.hallway_1ue_ma import Hallway1UEMA
 from rs.envs.hallway_1ue_ga import Hallway1UEGA
 from rs.envs.hallway_focus_ma import HallwayFocusMA
 from rs.envs.hallway_focus_ga import HallwayFocusGA
+from rs.envs.hallway_col_ma import HallwayColMA
 
 ENV_IDS = {
     "hallway_1ue_ma": Hallway1UEMA,
     "hallway_1ue_ga": Hallway1UEGA,
     "hallway_focus_ma": HallwayFocusMA,
     "hallway_focus_ga": HallwayFocusGA,
+    "hallway_col_ma": HallwayColMA,
 }
